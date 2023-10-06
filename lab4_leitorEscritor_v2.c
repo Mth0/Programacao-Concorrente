13d28d3892eb7ab3069ebff8569a2fa0a9d3a256
Matheus Ó <49344106+Mth0@users.noreply.github.com>
2023-10-06 19:31:37 -0300

    Add files via upload

---
+++ lab4_leitorEscritor_v2.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <pthread.h>
+#include "lab4_leitorEscritor_v2.h"
+
+pthread_mutex_t mutex;
+pthread_cond_t condLeitura;
+pthread_cond_t condEscrita;
+
+int leitor = 0;
+int escritor = 0;
+
+void inic(){
+	pthread_cond_init(&condLeitura, NULL);
+	pthread_cond_init(&condEscrita, NULL);
+}
+
+void destroi(){
+   pthread_cond_destroy(&condLeitura);
+   pthread_cond_destroy(&condEscrita);
+}
+
+//funcoes de leitura
+void entraLeitura(){
+	pthread_mutex_lock(&mutex);
+	while(escritor > 0){
+		pthread_cond_wait(&condLeitura, &mutex);
+	}
+	leitor++;
+	pthread_mutex_unlock(&mutex);
+}
+
+void saiLeitura(){
+	pthread_mutex_lock(&mutex);
+	leitor--;
+	if(leitor == 0){
+		pthread_cond_signal(&condEscrita);
+	}
+	pthread_mutex_unlock(&mutex);
+}
+
+
+//funcoes de escrita
+void entraEscrita(){
+	pthread_mutex_lock(&mutex);
+	while(leitor > 0 || escritor > 0){
+		pthread_cond_wait(&condEscrita, &mutex);
+	}
+	escritor++;
+	pthread_mutex_unlock(&mutex);
+}
+
+void saiEscrita(){
+	pthread_mutex_lock(&mutex);
+	escritor--;
+	if(escritor == 0){
+		pthread_cond_signal(&condEscrita);
+		pthread_cond_broadcast(&condLeitura);
+	}
+	pthread_mutex_unlock(&mutex);
+}
---
+++ lab4_leitorEscritor_v2.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void inic();
+void destroi();
+void entraLeitura();
+void saiLeitura();
+void entraEscrita();
+void saiEscrita();
